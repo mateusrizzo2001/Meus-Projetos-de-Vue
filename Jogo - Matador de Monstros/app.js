9f4d40117f5343b225f7035c347e396095bdea64
MateusRizzo <mateusrebellorizzo@gmail.com>
2022-08-15 14:10:13 -0300

    Matador de Monstros Completo

---
+++ Jogo - Matador de Monstros/app.js
@@ -0,0 +1,55 @@
+new Vue({
+    el: '#app',
+    data: {
+        isRunning: false,
+        playerLife: 10,
+        monsterLife: 0,
+        logs: []
+    },
+    computed: {
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
+    },
+    methods: {
+        startGame() {
+            this.isRunning = true,
+            this.playerLife = 100,
+            this.monsterLife = 100,
+            this.logs = []
+        },
+        attack(especial) {
+            this.hurt('playerLife', 7, 13, false, 'Monstro', 'Jogador', 'monster')
+            if (this.monsterLife > 0) {
+                this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+            }    
+        },
+        hurt(atr, min, max, especial, source, target, cls) {
+            const plus = especial ? 7 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[atr] = Math.max(this[atr] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
+        },
+        healAndHurt() {
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLog(`O Jogador ganhou força de ${heal}.`, 'player')
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value) 
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({text, cls})
+        }
+    },
+    watch: {
+        hasResult(value) {
+            if (value) this.isRunning = false
+        }
+    }
+})
